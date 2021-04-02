0ad239ff5951668180252db07d00ed5fcfb771c6
haris <haris_aksalic@hotmail.com>
2021-04-02 22:24:44 +0200

    push

---
+++ ex06/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PostRequestAsynAwait from './PostRequestAsyncAwait';
+
+function App() {
+  return (
+    <div className="App">
+     <PostRequestAsynAwait />
+    </div>
+  );
+}
+
+export default App;

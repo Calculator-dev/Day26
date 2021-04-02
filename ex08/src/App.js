fd81d1f8514a2614df7ebb59f579bc9e3051948e
haris <haris_aksalic@hotmail.com>
2021-04-02 23:31:12 +0200

    push

---
+++ ex08/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PostRequestSetHandling from './PostRequestSetHeaders';
+
+function App() {
+  return (
+    <div className="App">
+      <PostRequestSetHandling />
+    </div>
+  );
+}
+
+export default App;
